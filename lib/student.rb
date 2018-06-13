9b6f4bd619ea015641c44ab4ccdca0da1bb551d7
Shangni Hu <shangnihu@gmail.com>
2018-06-13 22:26:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,11 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def initialize(id=nil, name, album)
+    @id = id
+    @name = name
+    @album = album
+  end
 
 
 end
