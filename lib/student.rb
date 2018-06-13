67f01db2dba18218eb9fd6d119fb43ca05d90861
Shangni Hu <shangnihu@gmail.com>
2018-06-13 22:59:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,5 +58,11 @@ class Student
     Student.new(array[0], array[1], array[2])
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2]);
+  end
+
 
 end
